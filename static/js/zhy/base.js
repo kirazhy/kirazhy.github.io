265dcba97e4671be27caa57bdde2deeaea035a17
kirazhy <kirazhy@sina.com>
2017-04-21 15:00:32 +0800

    add icon

--- static/js/zhy/base.js
+++ static/js/zhy/base.js
@@ -7,11 +7,13 @@ var base = new Vue({
     navs: [
       {
         index: '1',
+        icon: 'fa-home',
         title: '首页',
         href: './template/homePage.html'
       },
       {
         index: '2',
+        icon: 'fa-address-card',
         title: '自我介绍',
         href: './template/resume.html'
       }
@@ -43,6 +45,14 @@ var base = new Vue({
     changeFrameHeight(event) {
       var mainheight = $(event.target).contents().find("body").height();
       $(event.target).height(mainheight);
+    },
+    //鼠标进入menu则i旋转
+    iSpin(event) {
+      $(event.target).find('i').addClass('fa-spin');
+    },
+    //鼠标离开menu则i停止
+    iStop(event) {
+      $(event.target).find('i').removeClass('fa-spin');
     }
   }
 });
